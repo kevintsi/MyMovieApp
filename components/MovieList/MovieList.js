8cb3a73a83b55afbcf9e0e87ab65d6b74370555c
Kévin TSI <kevtsiii@gmail.com>
2022-03-06 15:59:12 +0100

    MovieList component

---
+++ components/MovieList/MovieList.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MovieList = (movies) => {
+    return (
+        <View>
+            <Text>MovieList</Text>
+        </View>
+    )
+}
+
+export default MovieList
+
+const styles = StyleSheet.create({})
