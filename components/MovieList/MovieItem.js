c8ec958cc9fbd8eeb44b6a0de2e4ff3daa8ea6c0
Kévin TSI <kevtsiii@gmail.com>
2022-03-13 14:53:21 +0100

    Utilisation de FlatList à la place d'un map

--- components/MovieList/MovieItem.js
+++ components/MovieList/MovieItem.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 const MovieItem = (movie) => {
-    let base_movie = movie.movie
-    console.log("Movie contents ", base_movie.poster_path)
+    let base_movie = movie.movie.item
+    // console.log(`Movie : ${JSON.stringify(base_movie)}`)
+    // console.log("Movie contents ", base_movie.title)
+
     return (
         <View>
             <Text>{base_movie.title}</Text>
