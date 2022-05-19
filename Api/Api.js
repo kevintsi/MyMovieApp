5d8acc5024de8b7930241536435acec8a5df6d33
Kevin TSI <kevtsiii@gmail.com>
2022-05-19 14:21:55 +0200

    Rename prefix environment variable with REACT_APP

--- Api/Api.js
+++ Api/Api.js
@@ -1,7 +1,7 @@
 export const api_call = {
     getTopsRatedMoviesAPI: async (page = 1) => {
         try {
-            let res = await fetch(`${process.env.BASE_API}/movie/top_rated?api_key=${process.env.TMDB_API_KEY}&language=fr&page=${page}`)
+            let res = await fetch(`${process.env.REACT_APP_BASE_API}/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=fr&page=${page}`)
             return res.json()
         } catch (error) {
             console.log(`CALL API : Error occured when getting popular movies`)
@@ -10,7 +10,7 @@ export const api_call = {
     },
     getPopularMoviesAPI: async (page = 1) => {
         try {
-            let res = await fetch(`${process.env.BASE_API}/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=fr&page=${page}`)
+            let res = await fetch(`${process.env.REACT_APP_BASE_API}/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=fr&page=${page}`)
             return res.json()
         } catch (error) {
             console.log(`CALL API : Error occured when getting top rated movies`)
@@ -19,7 +19,7 @@ export const api_call = {
     },
     searchMovieAPI: async (title) => {
         try {
-            let res = await fetch(`${process.env.BASE_API}/search/movie?api_key=${process.env.TMDB_API_KEY}&language=fr&query=${title}`)
+            let res = await fetch(`${process.env.REACT_APP_BASE_API}/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=fr&query=${title}`)
             return res.json()
         } catch (error) {
             console.log(`CALL API : Error occured when searching for movie with th name : ${title}`)
@@ -29,7 +29,7 @@ export const api_call = {
 
     getMovieAPI: async (id) => {
         try {
-            let res = await fetch(`${process.env.BASE_API}/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=fr`)
+            let res = await fetch(`${process.env.REACT_APP_BASE_API}/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=fr`)
             return res.json()
         } catch (error) {
             console.log(`CALL API : Error occured when getting detail movie with id : ${id}`)
@@ -39,7 +39,7 @@ export const api_call = {
 
     getUpcomingMoviesAPI: async (page = 1) => {
         try {
-            let res = await fetch(`${process.env.BASE_API}/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=fr&page=${page}`)
+            let res = await fetch(`${process.env.REACT_APP_BASE_API}/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=fr&page=${page}`)
             return res.json()
         } catch (error) {
             console.log(`CALL API : Error occured when getting upcoming movies`)
@@ -49,7 +49,7 @@ export const api_call = {
 
     getMovieDetailAPI: async (movie_id) => {
         try {
-            let res = await fetch(`${process.env.BASE_API}/movie/${movie_id}?api_key=${process.env.TMDB_API_KEY}&language=fr`)
+            let res = await fetch(`${process.env.REACT_APP_BASE_API}/movie/${movie_id}?api_key=${process.env.TMDB_API_KEY}&language=fr`)
             return res.json()
         } catch (error) {
             console.log(`CALL API : Error occured when getting movie detail`)
